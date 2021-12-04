3bcc3210a2b8dfa6c524dfd493db95827f959aac
James <jeamsdean8917@gmail.com>
2021-12-04 21:23:43 +0900

    feat main : mb vh100 test

---
+++ page/Main.js
@@ -0,0 +1,54 @@
+import { useEffect, useLayoutEffect } from "react"
+import styles from '../styles/Home.module.css'
+
+export default function Main() {
+
+    useLayoutEffect(()=>{
+        let vh = window.innerHeight * 0.01; 
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+        
+    })
+    
+    return (
+        <main className={`${styles.main} mainwrap`}>
+            <h1 className={styles.title}>
+            Welcome to <a href="https://nextjs.org">James Dean.js!</a>
+            </h1>
+
+            <p className={styles.description}>
+              MB view 100 test
+            <code className={styles.code}>pages/index.js</code>
+            </p>
+
+            <div className={styles.grid}>
+            <a href="https://nextjs.org/docs" className={styles.card}>
+                <h2>Documentation &rarr;</h2>
+                <p>Find in-depth information about Next.js features and API.</p>
+            </a>
+
+            <a href="https://nextjs.org/learn" className={styles.card}>
+                <h2>Learn &rarr;</h2>
+                <p>Learn about Next.js in an interactive course with quizzes!</p>
+            </a>
+
+            <a
+                href="https://github.com/vercel/next.js/tree/master/examples"
+                className={styles.card}
+            >
+                <h2>Examples &rarr;</h2>
+                <p>Discover and deploy boilerplate example Next.js projects.</p>
+            </a>
+
+            <a
+                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                className={styles.card}
+            >
+                <h2>Deploy &rarr;</h2>
+                <p>
+                Instantly deploy your Next.js site to a public URL with Vercel.
+                </p>
+            </a>
+            </div>
+        </main>
+    )
+}
